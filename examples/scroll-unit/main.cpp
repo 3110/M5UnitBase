4e10c399c716460aa6e7a641e19e3cac170320c6
SAITO Tetsuya <saito.tetsuya@gmail.com>
2024-08-31 14:52:52 +0900

    サンプルコードの実装

--- examples/scroll-unit/main.cpp
+++ examples/scroll-unit/main.cpp
@@ -1,7 +1,17 @@
 #include "ScrollUnit.hpp"
 
+const char *TAG = "main";
 ScrollUnit scroll;
 bool pressed = false;
+
+struct encoder_t
+{
+    int32_t value;
+    int32_t prev;
+};
+
+encoder_t encoderValue;
+encoder_t incEncoderValue;
 
 void setup(void) {
     if (!scroll.begin(Wire1, RXD2, TXD2)) {
@@ -11,7 +21,19 @@ void setup(void) {
 
 void loop(void) {
     if (scroll.isButtonPressed(pressed) && pressed) {
-        ESP_LOGI("main", "Button pressed");
+        ESP_LOGI(TAG, "Button pressed");
     }
-    delay(100);
+    if (scroll.getEncoderValue(encoderValue.value) &&
+        encoderValue.prev != encoderValue.value) {
+        ESP_LOGI(TAG, "Encoder Value: %d", encoderValue.value);
+        encoderValue.prev = encoderValue.value;
+    }
+    if (scroll.getIncEncoderValue(incEncoderValue.value) &&
+        incEncoderValue.prev != incEncoderValue.value) {
+        if (incEncoderValue.value != 0) {
+            ESP_LOGI(TAG, "Inc Encoder Value: %d", incEncoderValue.value);
+        }
+        incEncoderValue.prev = incEncoderValue.value;
+        delay(10);
+    }
 }
